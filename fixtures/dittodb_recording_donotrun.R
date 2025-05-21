67df1282245d29e724c9b2267e23d960edfbb087
majazaloznik <maja.zaloznik@gmail.com>
2025-05-21 13:48:19 +0200

    Update fixture recording script to only record new series

--- fixtures/dittodb_recording_donotrun.R
+++ fixtures/dittodb_recording_donotrun.R
@@ -1,9 +1,7 @@
 ################################################################################
-##      One off code for recording the mock db fixtures and preparing
+##      Code for recording the mock db fixtures and preparing
 ##  the list of available time series. This code only works on the IMAD
-##  internal network, and once run is kept here only for archival purposes. 
-##           
-##                              DO NOT RUN
+##  internal network. 
 ##
 ################################################################################
 
@@ -17,8 +15,10 @@ source("R/helper_functions.R")
 con <- make_connection() 
 ## setup time series df
 ################################################################################
+# get current list of timeseries
+timeseries_old <- readRDS("data/available_timeseries.rds")
 
-# get list of timeseries from files
+# get list of timeseries from files which may have been updated
 sistat_series <- read.csv("data/indicators_sistat.csv", stringsAsFactors = FALSE)
 bs_series <- read.csv("data/indicators_bs.csv", stringsAsFactors = FALSE) |> 
         mutate(Sector = as.character(Sector),
@@ -34,6 +34,7 @@ timeseries <- bind_rows(
                        eurostat_series,
                        oecd_series)
 
+# get series ids and names from the database
 ids <- sql_get_series_id_from_series_code(
         timeseries$Dataset.code, con, schema = "platform")
 name_long_si <- sql_get_series_name_from_series(con, ids$id, schema = "platform")
@@ -41,16 +42,20 @@ name_long_si <- sql_get_series_name_from_series(con, ids$id, schema = "platform"
 timeseries$id <- ids$id
 timeseries$name_long_si <- name_long_si$name_long
 timeseries <- relocate(timeseries, c("id", "name_long_si"), .after = "Indicator")
+
+# save updated timeseries list
 saveRDS(timeseries, "data/available_timeseries.rds")
 
-# clean up old fixtures
-files <- list.files("fixtures/platform", pattern = NULL, full.names = TRUE)
-## capture mocks 
+# get new timeseries
+timeseries_new <- timeseries |> 
+        anti_join(timeseries_old)
+
+## capture mocks for new timeseries only
 ################################################################################
 start_db_capturing(path = "fixtures/")
 con <- make_connection() 
 
-outputs <- map(timeseries$id,  ~{
+outputs <- map(timeseries_new$id,  ~{
         sql_get_data_points_from_series_id(
                 con, 
                 .x)}) 
